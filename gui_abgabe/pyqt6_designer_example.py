549f011063d1089ebe4fb2e6a08a22204b200605
DodoMels <107034626+DodoMels@users.noreply.github.com>
2023-01-10 11:41:54 +0100

    exit gemacht und file save pendet ist noch der pfad und noch weiteres

--- gui_abgabe/pyqt6_designer_example.py
+++ gui_abgabe/pyqt6_designer_example.py
@@ -1,8 +1,14 @@
 import sys
 from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PIL import Image
+import PIL
+import os
+import pathlib
+
 
 
 from Mandelbrot.run_video import main
@@ -18,6 +24,7 @@ class MainWindow(QMainWindow):
         # uic.loadUi("gui_data.ui", self)
 
         self.resetValue = False
+        self.saveValue = False
 
         # Widgets from MainWIndow
         # Wenn der 'pushButton' geklickt wird
@@ -53,6 +60,38 @@ class MainWindow(QMainWindow):
         self.graphicsView.setToolTip('Bild des Mandelbrot')
         self.progressBar.setToolTip('Fortschritt der Berechnung')
 
+        #actionSave_as_png
+        self.actionSave_as_png.triggered.connect(self.handlesave)
+        self.actionquit.triggered.connect(self.closeEvent)
+
+#TODO Aktuellestes bild herausfinden
+    def handlesave(self):
+        print('hallo')
+        path = (pathlib.Path(__file__).parent.absolute())
+        print('path hat ', type(path))
+        print(path)
+
+        # creating a image object (main image)
+
+        #im1 = Image.open(r"Mandelbrot\eye0001.png")
+
+        # save a image using extension
+        #im1.save("mandelbrot1.png")
+
+#TODO Fragen wenn bild noch nicht gespeichert ob man es no will
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        button = QMessageBox.question(
+            self,
+            "Closeing App",
+            "Wollen Sie wirklich das Programm verlassen??",
+            buttons=QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+            defaultButton=QMessageBox.StandardButton.No,
+        )
+
+        if button == QMessageBox.StandardButton.Yes:
+            a0.accept()
+        else:
+            a0.ignore()
 
     def handleClickYes(self):
         self.resetValue = True
